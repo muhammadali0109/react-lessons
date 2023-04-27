6e82d0e07f0ca40781e9704c4515f88c20808318
muhammadali0109 <muhammadaliyev268@gmail.com>
2023-04-27 18:11:17 +0500

    lesson-2

---
+++ src/components/ComponentB.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable array-callback-return */
+
+
+const ComponentB = () => {
+  return (
+     <div className='hello'>
+      <h1>
+        hello
+      </h1>
+     </div>
+  )
+
+}
+
+
+
+export default ComponentB;
